1796a789a9f220ddf26f2d2ed979da03d4986de0
RyotaST <omamesan0621@icloud.com>
2021-01-11 16:49:15 +0900

    10進数変換プログラム作成

--- lib/rgb_self.rb
+++ lib/rgb_self.rb
@@ -1,3 +1,10 @@
 def to_hex(r, g, b)
   [r, g, b].inject('#') {|result, n| result + n.to_s(16).rjust(2, '0')}
 end
+
+def to_int(hex)
+  result = []
+  result << hex[1..2].to_i(16)
+  result << hex[3..4].to_i(16)
+  result << hex[5..6].to_i(16)
+end
--- test/rgb_test_self.rb
+++ test/rgb_test_self.rb
@@ -2,9 +2,15 @@ require 'minitest/autorun'
 require_relative '../lib/rgb_self.rb'
 
 class RgbTest < Minitest::Test
-  def test_rgb
+  def test_rgb_hex
     assert_equal '#000000', to_hex(0, 0, 0)
     assert_equal '#ffffff', to_hex(255, 255, 255)
     assert_equal '#237cdb', to_hex(35, 124, 219)
+  end
+
+  def test_rgb_int
+    assert_equal [0, 0, 0], to_int('#000000')
+    assert_equal [255, 255, 255], to_int('#ffffff')
+    assert_equal [35, 124, 219], to_int('#237cdb')
   end
 end
